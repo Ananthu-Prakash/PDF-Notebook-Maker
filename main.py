c6a54e6e86a0df329c6079f2e68229718e550152
ananthu <ananthu.pullayil@gmail.com>
2025-11-13 17:13:09 +0530

    Generate Pdf notpad based on Excel criteria

---
+++ main.py
@@ -0,0 +1,33 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit = "mm", format= "A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+df = pd.read_csv("topics.csv")
+
+for index,row in df.iterrows():
+    pdf.add_page()    
+    
+    pdf.set_font(family="times", style="B", size=24)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0, h=12, text=row["Topic"], ln=1, border=0)
+    
+    for i in range(20, 298, 10):
+        pdf.line(10,i,200,i)
+
+    pdf.ln(265)
+    pdf.set_font(family="times",style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h= 10, text= row["Topic"], align="R")
+
+    for i in range(row["Pages"]-1):
+        pdf.add_page()
+        pdf.ln(277)
+        pdf.set_font(family="times",style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h= 10, text= row["Topic"], align="R")
+
+        for i in range(20, 298, 10):
+            pdf.line(10,i,200,i)
+
+pdf.output("Output.pdf")
